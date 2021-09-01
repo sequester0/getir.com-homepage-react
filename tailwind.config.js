81c2919bb5255938a9a6ba0935a1275d7135212f
sequester0 <i.keremgumus@hotmail.com>
2021-09-01 15:58:03 +0300

    renamed and added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       backgroundColor: theme => ({
         ...theme('colors'),
         'brand-color': '#4C3398',
-        'hover-color': '#5D3EBC',
+        'primary-brand-color': '#5D3EBC',
       }),
       textColor: {
         'header': '#D7D6FC',
