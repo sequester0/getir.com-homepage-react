ff82b61b7a2e7ea5fe6fa1e21dbfa4b21fd36d6a
sequester0 <i.keremgumus@hotmail.com>
2021-09-01 18:02:15 +0300

    refactored and added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundColor: theme => ({
-        ...theme('colors'),
+      colors: {
         'brand-color': '#4C3398',
         'primary-brand-color': '#5D3EBC',
-        'secondary-brand-color': '#7849f7'
-      }),
-      gradientColorStops: theme => ({
-        ...theme('colors'),
-        'brand-color': '#4C3398',
-        'primary-brand-color': '#5D3EBC',
-        'secondary-brand-color': '#7849f7'
-      }),
-      textColor: {
+        'secondary-brand-color': '#7849f7',
+        'register-color': '#fafafa',
         'header': '#D7D6FC',
+        'brand-yellow': '#FFD300',
+        'facebook': '#3c5a99',
+        'facebook-button': '#E7EAF1'
       }
     },
   },
